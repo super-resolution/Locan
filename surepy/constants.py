f82064981423b98171857164205dca71b782b254
sdoose <soeren.doose@uni-wuerzburg.de>
2018-01-31 08:53:10 +0100

    added constants

--- surepy/constants.py
+++ surepy/constants.py
@@ -3,13 +3,24 @@ Constants to be used throughout the project
 """
 import os
 
+
+# set root directory for path operations
+
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+# Keys (i.e. names) for the most common LocData properties
 
 PROPERTY_KEYS = ['Position_x', 'Position_y', 'Position_z', 'Frame', 'Intensity', 'Local_background', 'Chi_square',
                  'Psf_sigma_x', 'Psf_sigma_y', 'Psf_sigma_z', 'Uncertainty_x', 'Uncertainty_y', 'Uncertainty_z',
                  'Channel', 'Index', 'Cluster_label', 'Two_kernel_improvement']
 
+# Keys (i.e. names) for the most common hulls
+
 HULL_KEYS = {'bounding_box', 'convex_hull', 'oriented_bounding_box', 'alpha_shape'}
+
+
+# Mapping column names in RapidSTORM files to LocData property keys
 
 RAPIDSTORM_KEYS = {
     'Position-0-0': 'Position_x',
@@ -20,6 +31,22 @@ RAPIDSTORM_KEYS = {
     'LocalBackground-0-0': 'Local_background',
     'TwoKernelImprovement-0-0': 'Two_kernel_improvement'
 }
+
+
+# Mapping column names in Zeiss Elyra files to LocData property keys
+
+ELYRA_KEYS = {
+    'Position-0-0': 'Position_x',
+    'Position-1-0': 'Position_y',
+    'ImageNumber-0-0': 'Frame',
+    'Amplitude-0-0': 'Intensity',
+    'FitResidues-0-0': 'Chi_square',
+    'LocalBackground-0-0': 'Local_background',
+    'TwoKernelImprovement-0-0': 'Two_kernel_improvement'
+}
+
+
+# Standard keys for metadata in LocData
 
 META_DICT = {
     'Identifier':'',

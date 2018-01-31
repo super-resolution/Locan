49d7ce0e36c0be7267fe98164226c4f84b15b9aa
sdoose <soeren.doose@uni-wuerzburg.de>
2018-01-31 09:38:51 +0100

    added load_Elyra_file

--- surepy/constants.py
+++ surepy/constants.py
@@ -11,7 +11,7 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Keys (i.e. names) for the most common LocData properties
 
-PROPERTY_KEYS = ['Position_x', 'Position_y', 'Position_z', 'Frame', 'Intensity', 'Local_background', 'Chi_square',
+PROPERTY_KEYS = ['Index', 'Position_x', 'Position_y', 'Position_z', 'Frame', 'Intensity', 'Local_background', 'Chi_square',
                  'Psf_sigma_x', 'Psf_sigma_y', 'Psf_sigma_z', 'Uncertainty_x', 'Uncertainty_y', 'Uncertainty_z',
                  'Channel', 'Index', 'Cluster_label', 'Two_kernel_improvement']
 
@@ -36,13 +36,20 @@ RAPIDSTORM_KEYS = {
 # Mapping column names in Zeiss Elyra files to LocData property keys
 
 ELYRA_KEYS = {
-    'Position-0-0': 'Position_x',
-    'Position-1-0': 'Position_y',
-    'ImageNumber-0-0': 'Frame',
-    'Amplitude-0-0': 'Intensity',
-    'FitResidues-0-0': 'Chi_square',
-    'LocalBackground-0-0': 'Local_background',
-    'TwoKernelImprovement-0-0': 'Two_kernel_improvement'
+    'Index': 'Index',
+    'First Frame': 'Frame',
+    'Number Frames': 'Frames_number',
+    'Frames Missing': 'Frames_missing',
+    'Position X [nm]': 'Position_x',
+    'Position Y [nm]': 'Position_y',
+    'Position Z [nm]': 'Position_z',
+    'Precision [nm]': 'Uncertainty',
+    'Number Photons': 'Intensity',
+    'Background variance': 'Local_background',
+    'Chi square': 'Chi_square',
+    'PSF half width [nm]': 'Psf_half_width',
+    'Channel': 'Channel',
+    'Z Slice': 'Slice_z'
 }
 
 
--- surepy/io/io_locdata.py
+++ surepy/io/io_locdata.py
@@ -42,8 +42,8 @@ def load_rapidSTORM_header(path):
             if name == 'identifier':
                 identifiers.append(value)
 
-    # turn identifiers into valuable Dataset_column_keys
-        column_keys = []
+    # turn identifiers into valuable LocData keys
+    column_keys = []
     for i in identifiers:
         column_keys.append(surepy.constants.RAPIDSTORM_KEYS[i])
 
@@ -77,6 +77,66 @@ def load_rapidSTORM_file(path, nrows=None, **kwargs):
     dat.meta['File path'] = str(path)
     dat.meta['Units'] = {'Position_x': 'nm', 'Position_y': 'nm'}
     dat.meta['History'] = [{'Method:': 'load_rapidSTORM_file', 'Parameter': [path, nrows]}]
+
+    return dat
+
+
+def load_Elyra_header(path):
+    """
+    Load xml header from a Zeiss Elyra single-molecule localization file and identify column names.
+
+    Parameters
+    ----------
+    path : str or Path object
+        File path for a rapidSTORM file to load.
+
+    Returns
+    -------
+    list of str
+        A list of valid dataset property keys as derived from the rapidSTORM identifiers.
+    """
+
+    with open(path) as file:
+        header = file.readline()
+
+    # list identifiers
+    identifiers = header.split("\t")
+
+    # turn identifiers into valuable LocData keys
+    column_keys = []
+    for i in identifiers:
+        column_keys.append(surepy.constants.RAPIDSTORM_KEYS[i])
+
+    return column_keys
+
+
+def load_Elyra_file(path, nrows=None, **kwargs):
+    """
+    Load data from a rapidSTORM single-molecule localization file.
+
+    Parameters
+    ----------
+    path : string or Path object
+        File path for a rapidSTORM file to load.
+    nrows : int, default: None
+        The number of localizations to load from file. None means that all available rows are loaded.
+
+    Returns
+    -------
+    Dataset
+        a new instance of Dataset with all localizations.
+    """
+    columns = load_Elyra_header(path)
+    dataframe = pd.read_table(path, sep=" ", skiprows=1, nrows=nrows, names=columns)
+
+    dat = LocData.from_dataframe(dataframe=dataframe, **kwargs)
+    dat.meta['State'] = 'raw'
+    dat.meta['Experimental setup'] =  {}
+    dat.meta['Experimental sample'] =  {}
+    dat.meta['File type'] = 'Elyra'
+    dat.meta['File path'] = str(path)
+    dat.meta['Units'] = {'Position_x': 'nm', 'Position_y': 'nm'}
+    dat.meta['History'] = [{'Method:': 'load_Elyra_file', 'Parameter': [path, nrows]}]
 
     return dat
 

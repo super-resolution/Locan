761756b5d3526da89437e20a3e1ebc5ccbd10381
sdoose <soeren.doose@uni-wuerzburg.de>
2018-02-02 15:39:19 +0100

    added test for 3D clustering

--- surepy/tests/data/test_clustering.py
+++ surepy/tests/data/test_clustering.py
@@ -16,11 +16,22 @@ def locdata():
     df = pd.read_csv(path, index_col=0)
     return LocData.from_dataframe(dataframe=df)
 
+@pytest.fixture()
+def locdata_3D():
+    path = Path(ROOT_DIR + '/tests/Test_data/five_blobs_3D.csv')
+    df = pd.read_csv(path, index_col=0)
+    return LocData.from_dataframe(dataframe=df)
 
 # tests
 
 def test_clustering_hdbscan(locdata):
     #print(locdata.data.head())
     clust = clustering_hdbscan(locdata, min_cluster_size = 5, allow_single_cluster = False)
+    #print(clust.data.head())
+    assert (len(clust) == 5)
+
+def test_clustering_hdbscan_3D(locdata_3D):
+    #print(locdata.data.head())
+    clust = clustering_hdbscan(locdata_3D, min_cluster_size = 5, allow_single_cluster = False)
     #print(clust.data.head())
     assert (len(clust) == 5)

d9100dd26ebda53c06956029417e530b8ae7a703
sdoose <soeren.doose@uni-wuerzburg.de>
2018-02-02 17:33:48 +0100

    changed test_data name

--- surepy/tests/test_dataset_io.py
+++ surepy/tests/test_dataset_io.py
@@ -5,11 +5,11 @@ import surepy.io.io_locdata as io
 import surepy.tests.test_data
 
 def test_get_correct_column_names_from_rapidSTORM_header():
-    columns = io.load_rapidSTORM_header(path=surepy.constants.ROOT_DIR + '/tests/test_data/rapidSTORM_dSTORM_data.txt')
+    columns = io.load_rapidSTORM_header(path=surepy.constants.ROOT_DIR + '/tests/test_data/rapidSTORM_dstorm_data.txt')
     assert (columns == ['Position_x', 'Position_y', 'Frame', 'Intensity', 'Chi_square', 'Local_background'])
 
 def test_loading_rapidSTORM_file():
-    dat = io.load_rapidSTORM_file(path=surepy.constants.ROOT_DIR + '/tests/test_data/rapidSTORM_dSTORM_data.txt', nrows=10)
+    dat = io.load_rapidSTORM_file(path=surepy.constants.ROOT_DIR + '/tests/test_data/rapidSTORM_dstorm_data.txt', nrows=10)
     assert (len(dat) == 10)
 
 def test_get_correct_column_names_from_Elyra_header():

3ab7e17c33e66a651f1a03f648bebbfa33e76d1f
sdoose <soeren.doose@uni-wuerzburg.de>
2018-02-23 10:37:28 +0100

    updated Localization_per_frame

--- surepy/analysis/localizations_per_frame.py
+++ surepy/analysis/localizations_per_frame.py
@@ -1,7 +1,22 @@
 """
 This module provides methods for analysis.
 """
+import matplotlib.pyplot as plt
+
 from surepy.analysis.analysis import Analysis
+
+
+def localizations_per_frame(data, normalization=None):
+    if normalization is None:
+        normalization_factor = 1
+    elif isinstance(normalization, str):
+        normalization_factor = data.properties[normalization]
+    elif isinstance(normalization, (int, float)):
+        normalization_factor = normalization
+    else:
+        raise TypeError('normalization should be None, a number or a valid property name.')
+
+    return data.data.groupby('Frame').size() / normalization_factor
 
 
 class Localizations_per_frame(Analysis):
@@ -30,44 +45,63 @@ class Localizations_per_frame(Analysis):
     """
     count = 0
 
-    def __init__(self, locdata, meta=None, normalization=None):
-        super().__init__(locdata, meta=meta, normalization=normalization)
+    def __init__(self, locdata, algorithm=localizations_per_frame, meta=None, **kwargs):
+        super().__init__()
+        self.locdata = locdata
+        self.algorithm = algorithm
+        self.parameter = kwargs
+        self.meta = self._init_meta(meta=meta)
+
+        self.results = None
+        self.secondary_results = None
+
+    def compute(self):
+        data = self.locdata  # take certain elements from locdata
+        self.results = self.algorithm(data=data, **self.parameter)  # some complicated algorithm
+        return None
 
 
-    def _compute_results(self, locdata, normalization=None):
+    def plot(self, ax=None, show=True, window=1):
+        '''
+        Provide plot as matplotlib axes object showing the running average of results over window size.
+        '''
+        if ax is None:
+            fig, ax = plt.subplots(nrows=1, ncols=1)
 
-        if normalization is None:
-            normalization_factor = 1
-        elif isinstance(normalization, str):
-            normalization_factor = locdata.properties[normalization]
-        elif isinstance(normalization, (int, float)):
-            normalization_factor = normalization
-        else:
-            raise TypeError('normalization should be None, a number or a valid property name.')
+        self.results.rolling(window=window, center=True).mean().plot(ax=ax)
 
-        return locdata.data.groupby('Frame').size() / normalization_factor
+        ax.set(title = 'Localizations per Frame',
+               xlabel = 'Frame',
+               ylabel = 'number of localizations'
+               )
+
+        ax.text(0.1,0.9,
+                "window = " + str(window),
+                transform = ax.transAxes
+                )
+
+        # show figure
+        if show:  # this part is needed if anyone wants to modify the figure
+            plt.show()
+
+        return None
 
 
-    def hist(self, ax, bins='auto'):
-        """ Provide histogram as matplotlib axes object showing hist(results). """
+    def hist(self, ax=None, show=True, bins='auto'):
+        '''
+        Provide histogram as matplotlib axes object showing hist(results).
+        '''
+        if ax is None:
+            fig, ax = plt.subplots(nrows=1, ncols=1)
+
         ax.hist(self.results.values, bins=bins, normed=True, log=False)
         ax.set(title = 'Localizations per Frame',
                xlabel = 'number of localizations',
                ylabel = 'PDF'
                )
 
-    def plot(self, ax, window=1):
-        """ Provide plot as matplotlib axes object showing the running average of results over window size. """
-        self.results.rolling(window=window, center=True).mean().plot(ax=ax)
-        ax.set(title = 'Localizations per Frame',
-               xlabel = 'Frame',
-               ylabel = 'number of localizations'
-               )
-        ax.text(0.1,0.9,
-                "window = " + str(window),
-                transform = ax.transAxes
-                )
+        # show figure
+        if show:  # this part is needed if anyone wants to modify the figure
+            plt.show()
 
-    def save_as_yaml(self):
-        """ Save results in a YAML format, that can e.g. serve as Origin import."""
-        raise NotImplementedError
+        return None

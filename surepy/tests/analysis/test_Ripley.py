2fa4f67257ce60361e70b120b399faa953d899ed
sdoose <soeren.doose@uni-wuerzburg.de>
2018-08-12 13:35:08 +0200

    added test for 3D Ripleys'k function

--- surepy/tests/analysis/test_Ripley.py
+++ surepy/tests/analysis/test_Ripley.py
@@ -13,27 +13,37 @@ from surepy.analysis import Ripleys_k_function, Ripleys_l_function, Ripleys_h_fu
 def locdata_blobs():
     return io.load_txt_file(path=surepy.constants.ROOT_DIR + '/tests/test_data/five_blobs.txt')
 
+@pytest.fixture()
+def locdata_blobs_3D():
+    return io.load_txt_file(path=surepy.constants.ROOT_DIR + '/tests/test_data/five_blobs_3D.txt')
+
 
 def test_Ripleys_k_function(locdata_blobs):
     radii = np.linspace(0,100,20)
     rhf = Ripleys_k_function(locdata_blobs, radii=radii).compute()
-    print(rhf.results[0:5])
+    #print(rhf.results[0:5])
+    assert (len(rhf.results) == len(radii))
+
+def test_Ripleys_k_function(locdata_blobs_3D):
+    radii = np.linspace(0,100,20)
+    rhf = Ripleys_k_function(locdata_blobs_3D, radii=radii).compute()
+    #print(rhf.results[0:5])
     assert (len(rhf.results) == len(radii))
 
 def test_Ripleys_k_function(locdata_blobs):
     radii = np.linspace(0,100,20)
     rhf = Ripleys_k_function(locdata_blobs, radii=radii, region_measure=1, other_locdata=locdata_blobs).compute()
-    print(rhf.results[0:5])
+    #print(rhf.results[0:5])
     assert (len(rhf.results) == len(radii))
 
 def test_Ripleys_l_function(locdata_blobs):
     radii = np.linspace(0,100,20)
     rhf = Ripleys_l_function(locdata_blobs, radii=radii).compute()
-    print(rhf.results[0:5])
+    #print(rhf.results[0:5])
     assert (len(rhf.results) == len(radii))
 
 def test_Ripleys_h_function(locdata_blobs):
     radii = np.linspace(0,100,20)
     rhf = Ripleys_h_function(locdata_blobs, radii=radii).compute()
-    print(rhf.results[0:5])
+    #print(rhf.results[0:5])
     assert (len(rhf.results) == len(radii))

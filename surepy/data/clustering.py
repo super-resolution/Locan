221c3094b70cc2bf22bcaf58eed0a8d03bef5687
sdoose <soeren.doose@uni-wuerzburg.de>
2018-08-13 17:27:05 +0200

    added dbscan method

--- surepy/data/clustering.py
+++ surepy/data/clustering.py
@@ -6,7 +6,9 @@ Methods for clustering localization data in LocData objects..
 
 import numpy as np
 import pandas as pd
-import hdbscan
+from hdbscan import HDBSCAN
+from sklearn.cluster import DBSCAN
+
 from surepy import LocData
 
 
@@ -51,7 +53,7 @@ def clustering_hdbscan(locdata, min_cluster_size = 5, allow_single_cluster = Fal
         A new LocData instance assembling all generated selections (i.e. localization cluster).
         If noise is True the first LocData object is a selection of all localizations that are defined as noise.
     """
-    labels = hdbscan.HDBSCAN(
+    labels = HDBSCAN(
         min_cluster_size=min_cluster_size,
         allow_single_cluster=allow_single_cluster,
         gen_min_span_tree=False
@@ -78,5 +80,55 @@ def clustering_hdbscan(locdata, min_cluster_size = 5, allow_single_cluster = Fal
     else:
         return collection
 
+
+# todo: set n_jobs as global constant.
+def clustering_dbscan(locdata, eps=20, min_samples=5, noise=False):
+    """
+    Cluster localizations in locdata using the dbscan clustering algorithm as implemented in sklearn.
+
+    Parameters
+    ----------
+    locdata : LocData
+        specifying the localization data on which to perform the manipulation.
+    eps : float
+        The maximum distance between two samples for them to be considered as in the same neighborhood.
+    min_samples : int
+        The number of samples in a neighborhood for a point to be considered as a core point.
+        This includes the point itself.
+    noise : bool
+        Flag indicating if the first cluster represents noise. If True a tuple of LocData objects is returned with
+        noise and cluster collection. If False a single LocData object is returned.
+
+    Returns
+    -------
+    LocData or tuple of LocData
+        A new LocData instance assembling all generated selections (i.e. localization cluster).
+        If noise is True the first LocData object is a selection of all localizations that are defined as noise.
+    """
+    labels = DBSCAN(
+        eps=eps, min_samples=min_samples, metric='euclidean', metric_params=None, algorithm='auto',
+        leaf_size=30, p=None, n_jobs=1
+    ).fit_predict(locdata.coordinates)
+
+    grouped = locdata.data.groupby(labels)
+
+    if noise:
+        selections = list(map(lambda x: LocData.from_selection(locdata=locdata, indices=x), grouped.indices.values()))
+        noise = selections[0]
+        collection = LocData.from_collection(*selections[1:])
+    else:
+        selections = list(map(lambda x: LocData.from_selection(locdata=locdata, indices=x), grouped.indices.values()))
+        collection = LocData.from_collection(*selections)
+
+    # metadata
+    del collection.meta.history[:]
+    collection.meta.history.add(name='clustering_dbscan',
+                         parameter='locdata={}, eps={}, min_samples={}'.format(
+                             locdata, eps, min_samples))
+
+    if noise:
+        return noise, collection
+    else:
+        return collection
 
 

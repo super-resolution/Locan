922953cf0fbc5ac33c3d512632c0dbe67f7b527f
sdoose <soeren.doose@uni-wuerzburg.de>
2018-09-20 08:39:07 +0200

    modified rescale image

--- surepy/render/render2d.py
+++ surepy/render/render2d.py
@@ -12,8 +12,7 @@ from skimage import exposure
 import surepy.data.properties.statistics
 
 
-# todo: fix problem with rescale
-def render2D(locdata, ax=None, show=True, bin_size=10, range='auto', rescale=(2, 98), cmap='magma'):
+def render2D(locdata, ax=None, show=True, bin_size=10, range='auto', rescale=True, cmap='magma'):
     """
     Render localization data into a 2D image by binning x,y-coordinates into regular bins.
 
@@ -29,14 +28,15 @@ def render2D(locdata, ax=None, show=True, bin_size=10, range='auto', rescale=(2,
         Flag indicating if plt.show() and the creation of a colorbar is active.
     bin_size : int or float
         x and y size of bins
-    range : [[min,max],[min,max]] or 'auto' or 'zero'
+    range : [[min,max],[min,max]] or 'auto' (default) or 'zero'
         defining the binned region by [min, max] ranges from input;
         for 'auto' by [min, max] ranges from data; for 'zero' by [0, max] ranges from data.
-    rescale : tuple, None, or 'equal'
+    rescale : True (default), tuple, None, or 'equal'
         rescale intensity values to be within percentile of max and min intensities
         (tuple with upper and lower bounds provided in percent).
-        For None intensity values are rescaled to the min and max possible values of the given representation.
+        For True intensity values are rescaled to the min and max possible values of the given representation.
         For 'equal' intensity values are rescaled by histogram equalization.
+        For None no rescaling occurs.
     cmap : str or Colormap instance
         The colormap used to map normalized data values to RGBA colors.
         We recommend to use one of 'viridis', 'plasma', 'magma', 'inferno', 'hot', 'hsv'.
@@ -90,15 +90,18 @@ def render2D(locdata, ax=None, show=True, bin_size=10, range='auto', rescale=(2,
     img = img.T  # to show image in the same format as scatter plot
 
     # contrast adjustment by equalization or rescaling
-    if isinstance(rescale, str):
-        if rescale == 'equal':
-            img = exposure.equalize_hist(img)
-    elif rescale is None:
+    if rescale is None or rescale is False:
+        pass
+    elif rescale is True:
         img = exposure.rescale_intensity(img) #scaling to min/max of img intensities
-    else:
+    elif isinstance(rescale, tuple):
         minmax = (img.min(), img.max())
         rescale_abs = tuple(np.multiply(np.divide(rescale, 100), (minmax[1] - minmax[0])) + minmax[0])
         img = exposure.rescale_intensity(img, in_range=rescale_abs)
+    elif rescale == 'equal':
+        img = exposure.equalize_hist(img)
+    else:
+        raise TypeError('Set rescale to tuple, None or "equal".')
 
     mappable = ax.imshow(img, origin='low', extent=[*range[0], *range[1]], cmap=cmap)
     ax.set(title='Image ({:.0f} nm per bin)'.format(bin_size),

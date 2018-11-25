901383557b76faae15b7e3a911e86466d70b7cd4
sdoose <soeren.doose@uni-wuerzburg.de>
2018-11-25 20:49:52 +0100

    changed sphinx conf

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../surepy'))
+#sys.path.insert(0, os.path.abspath('../surepy'))
+sys.path.insert(0, os.path.abspath('..'))
 
 
 
@@ -49,7 +50,7 @@ autosummary_generate = True  # Make _autosummary files and include them
 napoleon_google_docstring = True
 napoleon_numpy_docstring = True
 napoleon_include_init_with_doc = False
-napoleon_include_private_with_doc = True
+napoleon_include_private_with_doc = False
 napoleon_include_special_with_doc = False
 napoleon_use_admonition_for_examples = False
 napoleon_use_admonition_for_notes = False

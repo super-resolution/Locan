0e2b2ebab91d16bb676808cff345bdfb150e5108
sdoose <soeren.doose@uni-wuerzburg.de>
2019-01-31 23:25:15 +0100

    Test all constants

--- surepy/tests/test_constants.py
+++ surepy/tests/test_constants.py
@@ -1,7 +1,17 @@
-import surepy.constants
+from surepy.constants import (ROOT_DIR, PROPERTY_KEYS, HULL_KEYS, RAPIDSTORM_KEYS, ELYRA_KEYS, THUNDERSTORM_KEYS,
+                              N_JOBS, File_type)
+
+for sc in (ROOT_DIR, PROPERTY_KEYS, HULL_KEYS, RAPIDSTORM_KEYS, ELYRA_KEYS, THUNDERSTORM_KEYS,
+                              N_JOBS):
+    assert sc
+
+ft = File_type
+assert ft
+
+assert N_JOBS == 1
 
 def test_rapidstorm_keys_are_mapped_on_valid_property_keys():
-    for item in surepy.constants.RAPIDSTORM_KEYS.values():
-        assert (True if item in surepy.constants.PROPERTY_KEYS else False)
+    for item in RAPIDSTORM_KEYS.values():
+        assert (True if item in PROPERTY_KEYS else False)
 
 #todo tests for thunderstorm

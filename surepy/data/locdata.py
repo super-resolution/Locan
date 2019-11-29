Fix methods for hull computation

surepy/data/locdata.py
@@ -3,7 +3,6 @@
 A class to carry localization data.
 
 """
-
 import time
 import warnings
 
@@ -125,19 +124,43 @@ class LocData:
         # property for mean spatial coordinates (centroids)
         self.properties.update(dict(self.data[self.coordinate_labels].mean()))
 
-        # property for bounding box measures
-        if len(self.data.index) > 1:
+        # compute bounding box
+        self.bounding_box
+
+    def __del__(self):
+        """ Updating the counter upon deletion of class instance. """
+        self.__class__.count -= 1
+
+    def __len__(self):
+        """ Return the length of data, i.e. the number of elements (localizations or collection elements)."""
+        return len(self.data.index)
+
+    @property
+    def bounding_box(self):
+        if self._bounding_box is None:
             try:
-                self.bounding_box = surepy.data.hulls.BoundingBox(self.coordinates)
-                if self.bounding_box.region_measure:
-                    self.properties['region_measure_bb'] = self.bounding_box.region_measure
+                self._bounding_box = surepy.data.hulls.BoundingBox(self.coordinates)
+                if self._bounding_box.region_measure:
+                    self.properties['region_measure_bb'] = self._bounding_box.region_measure
                     self.properties['localization_density_bb'] = \
-                        self.meta.element_count / self.bounding_box.region_measure
-                if self.bounding_box.subregion_measure:
-                    self.properties['subregion_measure_bb'] = self.bounding_box.subregion_measure
+                        self.meta.element_count / self._bounding_box.region_measure
+                if self._bounding_box.subregion_measure:
+                    self.properties['subregion_measure_bb'] = self._bounding_box.subregion_measure
             except ValueError:
                 warnings.warn('Properties related to bounding box could not be computed.', UserWarning)
-                pass
+        return self._bounding_box
+
+    @property
+    def convex_hull(self):
+        if self._convex_hull is None:
+            try:
+                self._convex_hull = surepy.data.hulls.ConvexHull(self.coordinates)
+                self.properties['region_measure_ch'] = self._convex_hull.region_measure
+                self.properties['localization_density_ch'] = self.properties['localization_count'] \
+                                                                  / self._convex_hull.region_measure
+            except TypeError:
+                warnings.warn('Properties related to convex hull could not be computed.', UserWarning)
+        return self._convex_hull
 
     @property
     def region(self):
@@ -161,6 +184,29 @@ class LocData:
             if self._region.subregion_measure:
                 self.properties['subregion_measure'] = self._region.subregion_measure
 
+    @property
+    def data(self):
+        """ Return a pandas dataframe with all elements either copied from the reference or referencing the current
+        dataframe. """
+        if isinstance(self.references, LocData):
+            # we refer to the localization data by its index label, not position
+            # in other words we decided not to use iloc but loc
+            df = self.references.data.loc[self.indices]
+            df = pd.merge(df, self.dataframe, left_index=True, right_index=True, how='outer')
+            return df
+        else:
+            return self.dataframe
+
+    @property
+    def coordinates(self):
+        """ Return a numpy ndarray with all coordinate values. """
+        return self.data[self.coordinate_labels].values
+
+    @property
+    def centroid(self):
+        """ Return a numpy ndarray with coordinate values of the centroid
+        (being the property values for all coordinate labels)."""
+        return np.array([self.properties[coordinate_label] for coordinate_label in self.coordinate_labels])
 
     @classmethod
     def from_dataframe(cls, dataframe=pd.DataFrame(), meta=None):
@@ -196,7 +242,6 @@ class LocData:
             meta_.MergeFrom(meta)
 
         return cls(dataframe=dataframe, meta=meta_)
-
 
     @classmethod
     def from_selection(cls, locdata, indices=slice(0, None), meta=None):
@@ -256,7 +301,6 @@ class LocData:
         new_locdata.region = references.region
         return new_locdata
 
-
     @classmethod
     def from_collection(cls, locdatas, meta=None):
         """
@@ -294,7 +338,6 @@ class LocData:
             meta_.MergeFrom(meta)
 
         return cls(references=references, dataframe=dataframe, meta=meta_)
-
 
     @classmethod
     def concat(cls, locdatas, meta=None):
@@ -346,41 +389,28 @@ class LocData:
 
         return cls(references=references, dataframe=dataframe, meta=meta_)
 
-    @property
-    def data(self):
-        """ Return a pandas dataframe with all elements either copied from the reference or referencing the current
-        dataframe. """
-        if isinstance(self.references, LocData):
-            # we refer to the localization data by its index label, not position
-            # in other words we decided not to use iloc but loc
-            df = self.references.data.loc[self.indices]
-            df = pd.merge(df, self.dataframe, left_index=True, right_index=True, how='outer')
-            return df
-        else:
-            return self.dataframe
+    def reset(self, reset_index=False):
+        """
+        Reset hulls and properties. This is needed after the dataframe attribute has been modified in place.
 
-    @property
-    def coordinates(self):
-        """ Return a numpy ndarray with all coordinate values. """
-        return self.data[self.coordinate_labels].values
+        Parameters
+        ----------
+        reset_index : Bool
+            Flag indicating if the index is reset to integer values. If True the previous index values are discarded.
 
-    @property
-    def centroid(self):
-        """ Return a numpy ndarray with coordinate values of the centroid
-        (being the property values for all coordinate labels)."""
-        return np.array([self.properties[coordinate_label] for coordinate_label in self.coordinate_labels])
+        Returns
+        -------
+        LocData
+            The modified object
+        """
+        self._bounding_box = None
+        self._oriented_bounding_box = None
+        self._convex_hull = None
+        self._alpha_shape = None
+        if reset_index is True:
+            self.dataframe.reset_index(drop=True, inplace=True)
+        return self
 
-    def __del__(self):
-        """ Updating the counter upon deletion of class instance. """
-        self.__class__.count -= 1
-
-
-    def __len__(self):
-        """ Return the length of data, i.e. the number of elements (localizations or collection elements)."""
-        return len(self.data.index)
-
-
-    # todo change return value to self
     def reduce(self, reset_index=False):
         """
         Clean up references.
@@ -394,31 +424,36 @@ class LocData:
 
         Returns
         -------
-        Int
-            Flag set to 1 indicating if reference was changed, or set to 0 if no change was applied.
+        LocData
+            The modified object
         """
-        if self.references is None:
-            return_value = 0
-        elif isinstance(self.references, LocData):
+        if isinstance(self.references, LocData):
             self.dataframe = self.data
             self.indices = None
             self.references = None
-            return_value = 1
         else:
             raise ValueError('reference has undefined value.')
 
         if reset_index is True:
             self.dataframe.reset_index(drop=True, inplace=True)
 
-        return return_value
+        return self
 
+    def update_convex_hulls_in_references(self):
+        """
+        Compute the convex hull for each element in locdata.references and update locdata.dataframe.
+        """
+        if isinstance(self.references, list):
+            for reference in self.references:
+                reference.convex_hull  # request property to update reference._convex_hull
+            self.dataframe = pd.DataFrame([reference.properties for reference in self.references])
+        return self
 
     def print_meta(self):
         """
         Print Locdata.metadata.
         """
         print(text_format.MessageToString(self.meta))
-
 
     def print_summary(self):
         """

surepy/tests/data/test_hulls.py
@@ -1,13 +1,12 @@
 import pytest
 import numpy as np
-from surepy.data.hulls import BoundingBox, ConvexHull, _ConvexHullScipy, _ConvexHullShapely, \
-    update_convex_hulls_in_collection
-from surepy.data.cluster.clustering import cluster_dbscan
+from surepy.data.hulls import BoundingBox, ConvexHull, _ConvexHullScipy, _ConvexHullShapely
 
 
-def test_BoundingBox(locdata_2d):
+def test_BoundingBox_2d(locdata_2d):
     true_hull = np.array([[1, 1], [5, 6]])
     hull = BoundingBox(locdata_2d.coordinates)
+    assert hull.width.shape == (2,)
     assert(locdata_2d.properties['region_measure_bb'] == hull.region_measure == 20)
     assert(hull.subregion_measure == 18)
     np.testing.assert_array_equal(hull.hull, true_hull)
@@ -17,7 +16,8 @@ def test_BoundingBox(locdata_2d):
 def test_BoundingBox_3d(locdata_3d):
     true_hull = np.array([[1, 1, 1], [5, 6, 5]])
     hull = BoundingBox(locdata_3d.coordinates)
-    assert(locdata_3d.properties['region_measure_bb'] == hull.region_measure == 80)
+    assert hull.width.shape == (3,)
+    assert(hull.region_measure == 80)
     assert(hull.subregion_measure == 26)
     np.testing.assert_array_equal(hull.hull, true_hull)
     with pytest.raises(NotImplementedError):
@@ -34,17 +34,6 @@ def test_BoundingBox(locdata_empty, locdata_single_localization, locdata_non_sta
     locdata = eval(fixture_name)
     hull = BoundingBox(locdata.coordinates)
     assert hull.region_measure == expected
-
-
-def test_update_convex_hulls_in_collection(locdata_blobs_2d):
-    clust = cluster_dbscan(locdata_blobs_2d, eps=100, min_samples=4)
-    assert (len(clust) == 5)
-
-    new_collection = update_convex_hulls_in_collection(clust, copy=False)
-    assert new_collection is None
-    # print(clust.references[0].properties)
-    assert 'region_measure_ch' in clust.references[0].properties
-    assert 'localization_density_ch' in clust.references[0].properties
 
 
 def test_ConvexHullScipy(locdata_2d):
@@ -104,9 +93,11 @@ def test_ConvexHull_3d(locdata_3d):
     ('locdata_single_localization', 0)
 ])
 def test_ConvexHull(locdata_empty, locdata_single_localization,
-                     fixture_name, expected):
+                    fixture_name, expected):
     locdata = eval(fixture_name)
-    with pytest.raises(TypeError):
-        ConvexHull(locdata.coordinates, method='scipy')
-    with pytest.raises(TypeError):
-        ConvexHull(locdata.coordinates, method='shapely')
+    with pytest.warns(UserWarning):
+        hull = ConvexHull(locdata.coordinates, method='scipy')
+    assert np.isnan(hull.region_measure)
+    with pytest.warns(UserWarning):
+        hull = ConvexHull(locdata.coordinates, method='shapely')
+    assert np.isnan(hull.region_measure)

surepy/tests/data/test_locdata.py
@@ -4,14 +4,10 @@ import pandas as pd
 
 from surepy.data import metadata_pb2
 from surepy.data.locdata import LocData
-from surepy.data.rois import RoiRegion
-
-# todo check for selections that return zero data points
-# todo check for empty dataframe
-# todo check for single localization
+from surepy.data.region import RoiRegion
 
 
-# fixtures
+# fixtures for DataFrames (fixtures for LocData are defined in conftest.py)
 
 @pytest.fixture()
 def df_simple():
@@ -58,10 +54,20 @@ def test_LocData(df_simple):
     assert dat.coordinate_labels == ['position_x', 'position_y']
     assert dat.dimension == 2
     assert np.array_equal(dat.centroid, [2., 1.8])
+    assert dat.centroid[0] == dat.properties['position_x']
     for x, y in zip(dat.coordinates, [[0, 0], [0, 1], [1, 3], [4, 4], [5, 1]]):
         assert np.all(x == np.array(y))
     assert dat.meta.comment == COMMENT_METADATA.comment
     # assert dat.meta.identifier == '1'  # this test runs ok for this testing this function alone.
+    assert dat.bounding_box.region_measure == 20
+    assert 'region_measure_bb' in dat.properties
+    assert 'localization_density_bb' in dat.properties
+    assert dat.convex_hull.region_measure == 12.5
+    assert 'region_measure_ch' in dat.properties
+    assert 'localization_density_ch' in dat.properties
+    assert dat.region is None
+    dat.region = dat.bounding_box.region
+    assert dat.region.region_type == dat.bounding_box.region.region_type
 
 
 def test_LocData_empty(df_empty):
@@ -69,11 +75,13 @@ def test_LocData_empty(df_empty):
     assert len(dat) == 0
     assert dat.coordinate_labels == []
     assert dat.dimension == 0
+    # hulls are tested with locdata fixtures further down.
 
 
 def test_LocData_from_dataframe(df_simple):
     dat = LocData.from_dataframe(dataframe=df_simple, meta=COMMENT_METADATA)
     # print(dat.properties.keys())
+    assert dat.bounding_box is not None
     assert list(dat.properties.keys()) == ['localization_count', 'position_x', 'position_y', 'region_measure_bb',
                                            'localization_density_bb', 'subregion_measure_bb']
     assert len(dat) == 5
@@ -87,15 +95,15 @@ def test_LocData_from_dataframe_empty(df_empty):
     # print(dat.data)
 
 
-# this test is not running wihtin complete test run. But it works when run by itself.
+# this test is not running within complete test run. But it works when run by itself.
 # def test_LocData_count(df_simple):
 #     dat = LocData.from_dataframe(dataframe=df_simple, meta=COMMENT_METADATA)
-#     assert (LocData.count == 1)
+#     assert LocData.count == 1
 #     dat_2 = LocData.from_dataframe(dataframe=df_simple)
 #     assert(dat.properties == dat_2.properties)
-#     assert (LocData.count == 2)
-#     del(dat)
-#     assert (LocData.count == 1)
+#     assert LocData.count == 2
+#     del dat
+#     assert LocData.count == 1
 
 
 def test_LocData_from_dataframe_with_meta_dict(df_simple):
@@ -111,6 +119,7 @@ def test_LocData_from_selection(df_simple):
     assert (len(sel) == 3)
     assert (sel.references is dat)
     assert (sel.meta.comment == COMMENT_METADATA.comment)
+    assert dat.bounding_box.region_measure != sel.bounding_box.region_measure
 
     sel_sel = LocData.from_selection(locdata=sel, indices=[1, 3], meta={'comment': 'Selection of a selection.'})
     assert sel_sel.meta.comment == 'Selection of a selection.'
@@ -129,6 +138,14 @@ def test_LocData_from_collection(df_simple):
     assert (len(col.references) == 2)
     assert (len(col) == 2)
     assert (col.meta.comment == COMMENT_METADATA.comment)
+    assert set(col.data.columns) == {'localization_count', 'localization_density_bb', 'position_x', 'position_y',
+                                     'region_measure_bb', 'subregion_measure_bb'}
+
+    with pytest.warns(UserWarning):
+        col.update_convex_hulls_in_references()
+    assert set(col.data.columns) == {'localization_count', 'localization_density_bb', 'position_x', 'position_y',
+                                     'region_measure_bb', 'subregion_measure_bb',
+                                     'region_measure_ch', 'localization_density_ch'}
     # print(col.properties)
 
 
@@ -167,7 +184,6 @@ def test_LocData_concat(df_simple):
     sel_1.reduce()
     col = LocData.concat([sel_1, sel_2], meta=COMMENT_METADATA)
     assert len(col.references) == 2
-    sel_1.reduce()
     sel_2.reduce()
     col = LocData.concat([sel_1, sel_2], meta=COMMENT_METADATA)
     assert col.references is None
@@ -177,8 +193,7 @@ def test_LocData_reduce(df_simple):
     dat = LocData.from_dataframe(dataframe=df_simple, meta=COMMENT_METADATA)
     sel_1 = LocData.from_selection(locdata=dat, indices=[1, 3, 4], meta=COMMENT_METADATA)
     sel_2 = LocData.from_selection(locdata=dat, indices=[1, 3, 4], meta=COMMENT_METADATA)
-    flag = sel_1.reduce()
-    assert (flag == 1)
+    sel_1.reduce()
     assert (len(sel_1) == 3)
     assert (len(sel_1.data) == len(sel_2.data))
 
@@ -257,6 +272,19 @@ def test_standard_locdata_objects(
         fixture_name, expected):
     dat = eval(fixture_name)
     assert len(dat) == expected
+
+
+@pytest.mark.parametrize('fixture_name, expected', [
+    ('locdata_empty', pytest.warns(UserWarning)),
+    ('locdata_single_localization', pytest.warns(UserWarning)),
+])
+def test_locdata_hulls(
+        locdata_empty, locdata_single_localization,
+        fixture_name, expected):
+    dat = eval(fixture_name)
+    assert dat.bounding_box.region_measure == 0
+    with pytest.warns(UserWarning):
+        assert np.isnan(dat.convex_hull.region_measure)
 
 
 @pytest.mark.parametrize('fixture_name, expected', [
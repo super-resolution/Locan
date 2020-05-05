ad43c0ea3782aa72d9b7483036de24571861f830
sdoose <soeren.doose@uni-wuerzburg.de>
2020-05-05 16:18:04 +0200

    Fix test_QT_API

--- surepy/tests/gui/test_GUI_API.py
+++ surepy/tests/gui/test_GUI_API.py
@@ -1,10 +1,8 @@
 import os
 
-from surepy.constants import _has_napari
-if _has_napari: import napari
-
-from surepy.constants import QtBindings, QT_BINDINGS
+from surepy.constants import QT_BINDINGS
 
 
 def test_QT_API():
-    assert os.environ['QT_API'] == QT_BINDINGS.value
+    if 'QT_API' in os.environ:
+        assert os.environ['QT_API'] == QT_BINDINGS.value

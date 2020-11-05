187526e056fd0e558a9eb0b45583a02e6f2b28ca
sdoose <soeren.doose@uni-wuerzburg.de>
2020-11-05 20:10:58 +0100

    Add function range_from_collection()

--- surepy/data/properties/locdata_statistics.py
+++ surepy/data/properties/locdata_statistics.py
@@ -2,14 +2,18 @@
 
 Compute statistics for localization data.
 
-These values represent new properties of locdata.
+These values can represent new properties of locdata.
 
 """
+from collections import namedtuple
+
+import numpy as np
 import pandas as pd
+
 from surepy.data.locdata import LocData
 
 
-__all__ = ['statistics']
+__all__ = ['statistics', 'range_from_collection']
 
 
 def statistics(locdata, statistic_keys=('count', 'min', 'max', 'mean', 'median', 'std', 'sem')):
@@ -37,20 +41,51 @@ def statistics(locdata, statistic_keys=('count', 'min', 'max', 'mean', 'median',
     else:
         raise TypeError('locdata should be of type Locdata or Pandas.DataFrame.')
 
-    statistics = data.agg(statistic_keys)
+    statistics_ = data.agg(statistic_keys)
 
     if isinstance(locdata, pd.Series):
         p = data.name
         if isinstance(statistic_keys, str):
-            dict = {p + '_' + statistic_keys: statistics}
+            dict_ = {p + '_' + statistic_keys: statistics_}
         else:
-            dict = {p + '_' + s: statistics[s] for s in statistic_keys}
+            dict_ = {p + '_' + s: statistics_[s] for s in statistic_keys}
     else:
         if isinstance(statistic_keys, str):
             generator = (p for p in list(data))
-            dict = {p + '_' + statistic_keys: statistics[p] for p in generator}
+            dict_ = {p + '_' + statistic_keys: statistics_[p] for p in generator}
         else:
             generator = ((p, s) for p in list(data) for s in statistic_keys)
-            dict = {p + '_' + s: statistics[p][s] for p, s in generator}
+            dict_ = {p + '_' + s: statistics_[p][s] for p, s in generator}
 
-    return dict
+    return dict_
+
+
+def range_from_collection(locdata):
+    """
+    Compute the maximum range from all combined localizations for each dimension.
+
+    Parameters
+    ----------
+    locdata : LocData or list(LocData)
+        Collection of localization datasets.
+
+    Returns
+    -------
+    namedtuple
+        A namedtuple('Ranges', locdata.coordinate_labels) of namedtuple('Range', 'min max').
+    """
+    if isinstance(locdata.references, list):
+        locdatas = locdata.references
+    elif isinstance(locdata, (list, tuple)):
+        locdatas = locdata
+    else:
+        raise TypeError('locdata must contain a collection of Locdata objects.')
+
+    ranges = [locdata.bounding_box.hull for locdata in locdatas]
+    mins = np.array(ranges)[:, 0].min(axis=0)
+    maxs = np.array(ranges)[:, 1].max(axis=0)
+
+    Ranges = namedtuple('Ranges', locdata.coordinate_labels)
+    Range = namedtuple('Range', 'min max')
+    result = Ranges(*(Range(min_value, max_value) for min_value, max_value in zip(mins, maxs)))
+    return result
--- surepy/tests/data/test_properties_statistics.py
+++ surepy/tests/data/test_properties_statistics.py
@@ -2,46 +2,50 @@ import pytest
 import numpy as np
 import pandas as pd
 from surepy import LocData
-from surepy.data.properties import statistics
+from surepy.data.properties import statistics, range_from_collection
 
 
 @pytest.fixture()
 def locdata_simple():
-    dict = {
+    dict_ = {
         'position_x': [0, 0, 1, 4, 5],
         'position_y': [0, 1, 3, 4, 1]
     }
-    return LocData(dataframe=pd.DataFrame.from_dict(dict))
+    return LocData(dataframe=pd.DataFrame.from_dict(dict_))
 
 
 def test_statistics(locdata_simple):
     stat = statistics(locdata=locdata_simple)
     assert stat == {'position_x_count': 5.0, 'position_x_min': 0.0, 'position_x_max': 5.0, 'position_x_mean': 2.0,
-                   'position_x_median': 1.0, 'position_x_std': 2.3452078799117149,
-                   'position_x_sem': 1.0488088481701516, 'position_y_count': 5.0, 'position_y_min': 0.0,
-                   'position_y_max': 4.0, 'position_y_mean': 1.8, 'position_y_median': 1.0,
-                   'position_y_std': 1.6431676725154984, 'position_y_sem': 0.73484692283495345}
+                    'position_x_median': 1.0, 'position_x_std': 2.3452078799117149,
+                    'position_x_sem': 1.0488088481701516, 'position_y_count': 5.0, 'position_y_min': 0.0,
+                    'position_y_max': 4.0, 'position_y_mean': 1.8, 'position_y_median': 1.0,
+                    'position_y_std': 1.6431676725154984, 'position_y_sem': 0.73484692283495345}
+
 
 def test_statistics_from_dataframe(locdata_simple):
     stat = statistics(locdata=locdata_simple.data)
     assert stat == {'position_x_count': 5.0, 'position_x_min': 0.0, 'position_x_max': 5.0, 'position_x_mean': 2.0,
-                   'position_x_median': 1.0, 'position_x_std': 2.3452078799117149,
-                   'position_x_sem': 1.0488088481701516, 'position_y_count': 5.0, 'position_y_min': 0.0,
-                   'position_y_max': 4.0, 'position_y_mean': 1.8, 'position_y_median': 1.0,
-                   'position_y_std': 1.6431676725154984, 'position_y_sem': 0.73484692283495345}
+                    'position_x_median': 1.0, 'position_x_std': 2.3452078799117149,
+                    'position_x_sem': 1.0488088481701516, 'position_y_count': 5.0, 'position_y_min': 0.0,
+                    'position_y_max': 4.0, 'position_y_mean': 1.8, 'position_y_median': 1.0,
+                    'position_y_std': 1.6431676725154984, 'position_y_sem': 0.73484692283495345}
+
 
 def test_statistics_with_one_statfunction(locdata_simple):
-    stat = statistics(locdata=locdata_simple, statistic_keys=('min'))
+    stat = statistics(locdata=locdata_simple, statistic_keys='min')
     assert stat == {'position_x_min': 0, 'position_y_min': 0}
 
+
 def test_statistics_from_Series(locdata_simple):
-    stat = statistics(locdata=locdata_simple.data['position_x'], statistic_keys=('mean'))
+    stat = statistics(locdata=locdata_simple.data['position_x'], statistic_keys='mean')
     assert stat['position_x_mean'] == 2
     stat = statistics(locdata=locdata_simple.data['position_x'], statistic_keys=('min', 'max'))
     assert stat == {'position_x_min': 0, 'position_x_max': 5}
 
+
 def test_statistics_with_new_column(locdata_simple):
-    locdata_simple.dataframe = locdata_simple.dataframe.assign(new= np.arange(5))
+    locdata_simple.dataframe = locdata_simple.dataframe.assign(new=np.arange(5))
     stat = statistics(locdata=locdata_simple)
     # print(stat)
     assert stat == {'position_x_count': 5.0, 'position_x_min': 0.0, 'position_x_max': 5.0, 'position_x_mean': 2.0,
@@ -52,3 +56,12 @@ def test_statistics_with_new_column(locdata_simple):
                     'new_count': 5.0, 'new_min': 0.0, 'new_max': 4.0, 'new_mean': 2.0, 'new_median': 2.0,
                     'new_std': 1.5811388300841898, 'new_sem': 0.70710678118654757}
 
+
+def test_range_from_collection(locdata_3d):
+    collection = LocData.from_chunks(locdata_3d, n_chunks=2)
+    result = range_from_collection(collection)
+    print(result)
+    assert result.position_x.min == 1
+    assert result.position_x.max == 5
+    assert result.position_z.min == 1
+    assert result.position_z.max == 5

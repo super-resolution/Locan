0f7e4b0e6d05080145e5cf85f640b98ae6993d06
sdoose <soeren.doose@uni-wuerzburg.de>
2021-03-04 10:52:24 +0100

    Update version and changelog

--- surepy/__init__.py
+++ surepy/__init__.py
@@ -24,7 +24,7 @@ logging.getLogger(__name__).addHandler(logging.NullHandler())
 try:
     from surepy._version import version as __version__
 except ImportError:
-    __version__ = "0.6.0-not-installed"
+    __version__ = "0.6.0"
 
 from surepy.constants import *  # constants has to be imported before any others due to pyqt5/sidepy2 issues.
 from surepy.analysis import *

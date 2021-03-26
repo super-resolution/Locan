f1e8cae620b06f0cf96648263faefbeac9f141e2
sdoose <soeren.doose@uni-wuerzburg.de>
2021-03-26 10:49:41 +0100

    Update version and changelog

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ since PEP 517 doesn’t support editable installs.
 from setuptools import setup
 setup(
     use_scm_version={"write_to": "surepy/_version.py",
-                     "fallback_version": "0.7-fallback"}
+                     "fallback_version": "0.7"}
     )

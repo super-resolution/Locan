996a07f4242eec2bf152de43b2d1cea7155667ac
sdoose <soeren.doose@uni-wuerzburg.de>
2021-04-22 18:41:45 +0200

    Change verbose and extra flag in script show_versions

--- locan/scripts/script_show_versions.py
+++ locan/scripts/script_show_versions.py
@@ -22,10 +22,10 @@ from locan.utils.system_information import show_versions as sc_show_versions
 
 
 def _add_arguments(parser):
-    parser.add_argument('-v', '--verbose', dest='verbose', action='store_false', default='store_true',
-                        help='Do not include information on node and executable path.')
-    parser.add_argument('-e', '--extra', dest='extra', action='store_false', default='store_true',
-                        help='Do not include extra dependencies as specified in setup.py.')
+    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true',
+                        help='Include information on node and executable path.')
+    parser.add_argument('-e', '--extra', dest='extra', action='store_true',
+                        help='Include extra dependencies as specified in setup.py.')
     parser.add_argument('-o', '--other', dest='other', type=str, nargs='*',
                         help='Include other module names.')
 
@@ -35,6 +35,7 @@ def main(args=None):
     parser = argparse.ArgumentParser(description='Show system information and dependency versions.')
     _add_arguments(parser)
     returned_args = parser.parse_args(args)
+    print(returned_args)
     sc_show_versions(verbose=returned_args.verbose, extra_dependencies=returned_args.extra,
                   other_dependencies=returned_args.other)
 

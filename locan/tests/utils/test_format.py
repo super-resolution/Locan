fa77099d914eacd937eb9a0d383566c2665dfbfd
sdoose <soeren.doose@uni-wuerzburg.de>
2021-11-24 11:26:37 +0100

    Fix test_time_string

--- locan/tests/utils/test_format.py
+++ locan/tests/utils/test_format.py
@@ -6,4 +6,6 @@ from locan.utils.format import _time_string
 def test_time_string():
     time_ = 1000000000  # result from time.time()
     string_ = _time_string(time_)
-    assert string_ == '2001-09-09 03:46:40 +0200'
+    # assert string_ == '2001-09-09 03:46:40 +0200'  # breaks in different time zones
+    assert string_.startswith("2001-09-09 ")
+    assert ":46:40 " in string_

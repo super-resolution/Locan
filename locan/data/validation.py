cfca3d6c0cac406410321e5cf32209ab0159d1ac
sdoose <soeren.doose@uni-wuerzburg.de>
2022-10-12 11:57:29 +0200

    Add validation.py

---
+++ locan/data/validation.py
@@ -0,0 +1,52 @@
+"""
+
+Validate localization data.
+
+This module provides functions to validate LocData properties.
+
+"""
+from __future__ import annotations
+
+from collections.abc import Iterable
+
+from locan.data.locdata import LocData
+
+__all__ = []
+
+
+def _check_loc_properties(
+    locdata: LocData, loc_properties: str | Iterable | None = None
+) -> list[str]:
+    """
+    Check that loc_properties are valid properties in locdata.
+
+    Parameters
+    ----------
+    locdata
+        Localization data
+    loc_properties
+        LocData property names. If None coordinate labels are used.
+
+    Returns
+    -------
+    list[str]
+        Valid localization property names
+    """
+    if loc_properties is None:  # use coordinate_labels
+        labels = locdata.coordinate_labels.copy()
+    elif isinstance(loc_properties, str):
+        if loc_properties not in locdata.data.columns:
+            raise ValueError(
+                f"{loc_properties} is not a valid property in locdata.data."
+            )
+        labels = [loc_properties]
+    elif isinstance(loc_properties, (tuple, list)):
+        labels = list(loc_properties)
+        for loc_property in loc_properties:
+            if loc_property not in locdata.data.columns:
+                raise ValueError(
+                    f"{loc_property} is not a valid property in locdata.data."
+                )
+    else:
+        raise ValueError(f"{loc_properties} is not a valid property in locdata.data.")
+    return labels
---
+++ locan/tests/data/test_validate.py
@@ -0,0 +1,12 @@
+import pytest
+
+from locan.data.validation import _check_loc_properties
+
+
+def test__check_loc_properties(locdata_2d):
+    assert _check_loc_properties(locdata_2d) == locdata_2d.coordinate_labels
+    assert _check_loc_properties(locdata_2d, loc_properties=["intensity"]) == [
+        "intensity"
+    ]
+    with pytest.raises(ValueError):
+        _check_loc_properties(locdata_2d, loc_properties=["undefined"])

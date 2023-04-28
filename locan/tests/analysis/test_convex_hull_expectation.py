1b4d2b284fd756483700ce58541bc6758f71488e
sdoose <soeren.doose@uni-wuerzburg.de>
2023-04-28 11:40:09 +0200

    refactor(analysis): change convex_hull_expectation

--- locan/tests/analysis/test_convex_hull_expectation.py
+++ locan/tests/analysis/test_convex_hull_expectation.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 import boost_histogram as bh
 import matplotlib.pyplot as plt
 import numpy as np
@@ -90,45 +92,81 @@ class TestConvexHullPropertyExpectation:
         plt.close("all")
 
     def test_compute(self, locdata_2d):
+        locdata_2d = deepcopy(locdata_2d)
         collection = LocData.from_collection(locdatas=[locdata_2d, locdata_2d])
         cpe = ConvexHullExpectation().compute(locdata=collection)
+        # print(cpe.results.values)
+        # print(cpe.results.grouped)
         assert isinstance(cpe.results, ConvexHullExpectationResults)
-        assert cpe.results.convex_hull_property[
-            "region_measure_ch"
-        ].tolist() == pytest.approx([14, 14], rel=0.1)
-        assert cpe.results.convex_hull_property_mean is not None
-        assert cpe.results.convex_hull_property_std is not None
-        assert cpe.results.convex_hull_property_expectation is not None
+        assert cpe.results.values.index.tolist() == collection.data.index.tolist()
+
+        assert cpe.results.values["region_measure_ch"].tolist() == pytest.approx(
+            [14, 14], rel=0.1
+        )
+
+        assert cpe.results.values.columns.tolist() == [
+            "localization_count",
+            "region_measure_ch",
+            "expectation",
+            "value_to_expectation_ratio",
+        ]
+        assert cpe.results.grouped.index.tolist() == [6]
+        assert cpe.results.grouped.columns.tolist() == [
+            "region_measure_ch_mean",
+            "region_measure_ch_std",
+            "expectation",
+            "expectation_std_pos",
+            "expectation_std_neg",
+        ]
+        assert cpe.results.grouped.expectation.isna().all()
 
         cpe.plot()
         # plt.show()
-
-        cpe = ConvexHullExpectation(
-            convex_hull_property="subregion_measure_ch", expected_variance=100
-        ).compute(locdata=collection)
-        assert isinstance(cpe.results, ConvexHullExpectationResults)
-        assert cpe.results.convex_hull_property[
-            "subregion_measure_ch"
-        ].tolist() == pytest.approx([14, 14], rel=0.1)
-        assert cpe.results.convex_hull_property_mean is not None
-        assert cpe.results.convex_hull_property_std is not None
-
-        cpe.plot()
-        # plt.show()
-
         cpe.hist()
         # plt.show()
 
-        cpe.hist(n_bins=(20, 50), bin_range=((3, 100), (1, 1_000)))
-        # plt.show()
+        plt.close("all")
 
-        axes = bh.axis.AxesTuple(
-            (
-                bh.axis.Regular(20, 3, 100, transform=bh.axis.transform.log),
-                bh.axis.Regular(50, 1, 1_000, transform=bh.axis.transform.log),
-            )
+    def test_compute_with_expectation(self, locdata_2d):
+        locdata_2d = deepcopy(locdata_2d)
+        collection = LocData.from_collection(locdatas=[locdata_2d, locdata_2d])
+        cpe = ConvexHullExpectation(
+            convex_hull_property="subregion_measure_ch", expected_variance=100
+        ).compute(locdata=collection)
+        # print(cpe.results.values)
+        # print(cpe.results.grouped)
+        assert cpe.results.values.index.tolist() == collection.data.index.tolist()
+        assert cpe.results.values.columns.tolist() == [
+            "localization_count",
+            "subregion_measure_ch",
+            "expectation",
+            "value_to_expectation_ratio",
+        ]
+        assert cpe.results.grouped.index.tolist() == [6]
+        assert cpe.results.grouped.columns.tolist() == [
+            "subregion_measure_ch_mean",
+            "subregion_measure_ch_std",
+            "expectation",
+            "expectation_std_pos",
+            "expectation_std_neg",
+        ]
+
+        assert cpe.results.grouped.expectation.tolist() == pytest.approx(
+            [321.0], rel=0.1
         )
-        cpe.hist(bins=axes)
+        assert cpe.results.grouped.expectation_std_pos.tolist() == pytest.approx(
+            [198.0], rel=0.1
+        )
+        assert cpe.results.grouped.expectation_std_neg.tolist() == pytest.approx(
+            [132], rel=0.1
+        )
+        assert cpe.results.values.value_to_expectation_ratio.tolist() == pytest.approx(
+            [0.046096, 0.046096], rel=1e-3
+        )
+
+        cpe.plot()
+        # plt.show()
+        cpe.hist()
         # plt.show()
 
         plt.close("all")
@@ -137,6 +175,7 @@ class TestConvexHullPropertyExpectation:
 @pytest.mark.visual
 class TestConvexHullPropertyExpectationVisual:
     def test_compute_for_visualization(self, locdata_blobs_2d):
+        locdata_blobs_2d = deepcopy(locdata_blobs_2d)
         grouped = locdata_blobs_2d.data.groupby("cluster_label")
         collection = LocData.from_collection(
             locdatas=[
@@ -146,14 +185,8 @@ class TestConvexHullPropertyExpectationVisual:
         )
         cpe = ConvexHullExpectation(expected_variance=400).compute(locdata=collection)
 
-        print(cpe.results.convex_hull_property)
-        print(cpe.results.convex_hull_property_mean)
-        print(cpe.results.convex_hull_property_std)
-
-        assert isinstance(cpe.results, ConvexHullExpectationResults)
-        assert cpe.results.convex_hull_property is not None
-        assert cpe.results.convex_hull_property_mean is not None
-        assert cpe.results.convex_hull_property_std is not None
+        print(cpe.results.values.columns)
+        print(cpe.results.grouped)
 
         cpe.plot()
         plt.show()

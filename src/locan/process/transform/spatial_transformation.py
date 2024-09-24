a17e5f7b53741cfac6e1829c108cb10cf2328047
sdoose <soeren.doose@uni-wuerzburg.de>
2024-09-24 14:50:36 +0200

    refactor: add check for runtime numpy version in open3d functions.

--- src/locan/process/transform/spatial_transformation.py
+++ src/locan/process/transform/spatial_transformation.py
@@ -21,7 +21,7 @@ import pandas as pd
 from locan.data.locdata import LocData
 from locan.data.metadata_utils import _modify_meta
 from locan.data.validation import _check_loc_properties
-from locan.dependencies import HAS_DEPENDENCY
+from locan.dependencies import HAS_DEPENDENCY, needs_package_version
 
 if HAS_DEPENDENCY["open3d"]:
     import open3d as o3d
@@ -132,6 +132,7 @@ def _homogeneous_matrix(
     return matrix_
 
 
+@needs_package_version(package="numpy", major="1")
 def _transform_affine_open3d(
     points: npt.ArrayLike,
     matrix: npt.ArrayLike | None = None,

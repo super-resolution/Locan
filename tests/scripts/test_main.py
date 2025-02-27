32227bafb5feec7622a52745200cd337057e8f72
sdoose <soeren.doose@uni-wuerzburg.de>
2025-02-27 22:43:39 +0100

    refactor: dockerfiles for new tests layout

--- tests/scripts/test_main.py
+++ tests/scripts/test_main.py
@@ -11,19 +11,19 @@ def test_entrypoint_from_sys():
 
 def test_main():
     exit_status = subprocess.run(  # noqa S603
-        "locan --help", capture_output=True, encoding="utf-8"
+        ["locan", "--help"], capture_output=True, encoding="utf-8"
     )
     assert exit_status.stdout.startswith("usage:")
     exit_status.check_returncode()
 
     exit_status = subprocess.run(  # noqa S603
-        "locan --version", capture_output=True, encoding="utf-8"
+        ["locan", "--version"], capture_output=True, encoding="utf-8"
     )
     assert exit_status.stdout.startswith("locan version")
     exit_status.check_returncode()
 
     exit_status = subprocess.run(  # noqa S603
-        "locan --info", capture_output=True, encoding="utf-8"
+        ["locan", "--info"], capture_output=True, encoding="utf-8"
     )
     for item in ["Locan:", "Python:", "System:", "Python dependencies:"]:
         assert item in exit_status.stdout
